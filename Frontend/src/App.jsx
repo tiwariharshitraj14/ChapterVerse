6d820d8bb295894f70dac1f0e4e0bff88d0db8d4
Harsh <tiwariharshitraj28@gmail.com>
2025-01-13 13:18:20 +0530

    ✨ feat: Add Navbar component and integrate DaisyUI
    - Add DaisyUI plugin to Tailwind config
    - Create Navbar component structure
    - Integrate Navbar into App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar />
   )
 }
 
